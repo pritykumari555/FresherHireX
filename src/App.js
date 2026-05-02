0219db08bdbc3b30f473e090a0118b35a005a6b3
pritykumari555 <pritykumari5946@example.com>
2026-05-02 14:21:34 +0530

    fixed vercel build error

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css'
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home   from './Pages/Home'
 import Login  from './Pages/Login'
 import Signup from './Pages/Signup'
